c78a469deb1d0bb0e42399500094c1843552c1c3
Alexandr Yakovlev <alexandr.yakovlev228@gmail.com>
2021-02-24 23:47:41 +0300

    доработал проверку в математических функциях

--- src/changeNumOnVal.js
+++ src/changeNumOnVal.js
@@ -1,8 +1,22 @@
 const changeNumOnVal = (str, res) => {
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 0; j < res.length; j++) {
-      if (res[j]['num'].toLowerCase() === str[i]) {
-        str[i] = res[j]['val'];
+  switch (res.length) {
+    case 1:
+      str = res[0]['val'];
+      break;
+    case 2:
+      res.every(obj => parseInt(obj.val)) ? setValue() : str = '#ИМЯ?';
+      break;
+    default:
+      setValue();
+      break;
+  }
+
+  function setValue() {
+    for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < res.length; j++) {
+        if (res[j]['num'].toLowerCase() === str[i]) {
+          str[i] = res[j]['val'];
+        }
       }
     }
   }
